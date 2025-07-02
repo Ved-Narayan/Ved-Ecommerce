c4e17d4f8e754d6928c544bab149f5f05d259492
Ved Narayan <114128689+Ved-Narayan@users.noreply.github.com>
2025-07-02 15:23:28 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import NewsletterBox from '../components/NewsletterBox'
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-white min-h-screen">
         <Hero/>
         <LatestCollection/>
         <BestSeller />
